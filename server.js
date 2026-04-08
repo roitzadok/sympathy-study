8d2bff4a10bcf190c0f222eec8f24b9e5fc12872
roi tzadok <roitzadok1@walla.com>
2026-04-08 16:54:36 +0300

    server change

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 8080;
 app.use(express.static(path.join(__dirname, 'dist')));
 
 // Fallback to index.html for SPA routing
-app.get('*', (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
